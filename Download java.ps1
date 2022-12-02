f89f20124fa07012ab92a8bc704552819b33ba8d
anawesomeprogrammer <90028290+anawesomeprogrammer@users.noreply.github.com>
2022-12-02 19:57:24 +0530

    Create Download java.ps1

---
+++ Download java.ps1
@@ -0,0 +1,2 @@
+wget https://download.oracle.com/java/19/latest/jdk-19_windows-x64_bin.zip
+curl https://download.oracle.com/java/19/latest/jdk-19_windows-x64_bin.zip -O "jdk-19_windows-x64_bin.zip" 
